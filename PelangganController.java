736da5e3fbb9d71ca8b1ec31b2ac45e409ff9567
Adam Firdaus D <72844680+eDemson88@users.noreply.github.com>
2021-02-13 19:46:34 +0700

    Add files via upload

---
+++ PelangganController.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project.lavanderia.controller;
+
+import javax.swing.JOptionPane;
+import project.lavanderia.model.PelangganModel;
+import project.lavanderia.view.TambahAntrianView;
+
+/**
+ *
+ * @author Dean
+ */
+public class PelangganController {
+    private PelangganModel model;
+
+    public void setModel(PelangganModel model) {
+        this.model = model;
+    }
+    
+    public void insertPelangganView (TambahAntrianView view) {
+        String nama = view.getTxtNama().getText();
+        String alamat = view.getTxtAlamat().getText();
+        String telp = view.getTxtTelp().getText();
+        String jenis = (String) view.getCmbJenis().getSelectedItem();
+        String berat = view.getTxtBerat().getText();
+        String harga = view.getTxtHarga().getText();
+        
+        if (nama.trim().equals("")){
+            JOptionPane.showMessageDialog(view, "Nama Masih Kosong");
+        } else if (nama.length() > 255) {
+            JOptionPane.showMessageDialog(view, "Nama depan tidak boleh lebih dari 255");
+        } else if (telp.length() > 12) {
+            JOptionPane.showMessageDialog(view, "Nomor telepon tidak boleh lebih dari 12 digit");
+        } else if (view.getTxtHarga().getText().equals("")) {
+            JOptionPane.showMessageDialog(view, "Klik 'cek harga' dahulu");
+        } else {
+            model.setNama(nama);
+            model.setAlamat(alamat);
+            model.setTelp(telp);
+            model.setJenis(jenis);
+            double vberat = Double.parseDouble(berat);
+            model.setBerat(vberat);
+            double vharga = Double.parseDouble(harga);
+            model.setHarga(vharga);
+            try {
+                model.insertPelanggan();
+                JOptionPane.showMessageDialog(view, "Data Pelanggan Berhasil Ditambahkan");
+                model.resetPelanggan();
+            } catch (Throwable throwable) {
+                JOptionPane.showMessageDialog(view, new Object[]{
+                    "Terjadi error di database dengan pesan ", throwable.getMessage()
+                });
+            }
+        }
+    }
+}
